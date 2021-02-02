ffefba431cec28faeef7b58d96ffa42b4855531d
uimarshall <uimarshall@gmail.com>
2021-02-02 16:56:42 +0100

    Add Sidebar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,17 @@
-import React from "react"
+import React, { useState } from "react"
 // import "../css/main.css"
 import Navbar from "./Navbar"
 import Sidebar from "./Sidebar"
 import Footer from "./Footer"
 const Layout = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen)
+  }
   return (
     <>
-      <Navbar />
-      <Sidebar />
+      <Navbar toggleSidebar={toggleSidebar} />
+      <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
       {children}
       <Footer />
     </>
